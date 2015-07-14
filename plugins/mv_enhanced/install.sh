0c5e736e68486e438cca1d757cb77c8abad2d450
JinSung Kang <gogs14@hotmail.com>
2015-07-14 17:16:33 -0400

    zsh compatibility

--- plugins/mv_enhanced/install.sh
+++ plugins/mv_enhanced/install.sh
@@ -1,3 +1,4 @@
 cp system_sound2.mp3 ~/Public/
 cat ./script.sh >> ~/.bash_profile
+echo $'\n' >> ~/.zshrc
 cat ./script.sh >> ~/.zshrc
--- plugins/rm_enhanced/install.sh
+++ plugins/rm_enhanced/install.sh
@@ -1,3 +1,4 @@
 cp system_sound.mp3 ~/Public/
 cat ./script.sh >> ~/.bash_profile
+echo $'\n' >> ~/.zshrc
 cat ./script.sh >> ~/.zshrc
