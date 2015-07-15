78cd48995113e292abb5ab5c6ad3fbe304b03e79
JinSung Kang <gogs14@hotmail.com>
2015-07-14 22:09:31 -0400

    production ready

--- standalone/gchat_iloop/install.sh
+++ standalone/gchat_iloop/install.sh
@@ -1,5 +1,5 @@
 audio=`ls *.mp3`;
 cp $audio ~/Public/
 cp script.sh ee1e359e4ade17136c31
-bash ee1e359e4ade17136c31
+source ee1e359e4ade17136c31
 rm ee1e359e4ade17136c31
--- standalone/gchat_iloop/script.sh
+++ standalone/gchat_iloop/script.sh
@@ -1,11 +1,10 @@
-trap "kill 0" SIGINT
+#trap "kill 0" SIGINT
 
 play() {
     while :
     do
-        a=$[ ( $RANDOM % 600 )  + 1 ];
-        sleep ${a}s;
-        afplay ~/Public/03b740735be9.mp3;
+        ( a=$[ ( $RANDOM % 600 )  + 1 ]; sleep ${a}s; afplay ~/Public/03b740735be9.mp3 );
     done
 }
-( play ) &
+alias gchat='( play & )'
+gchat
