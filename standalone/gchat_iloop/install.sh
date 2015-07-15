e5dfa121ae56b45e7fe8afc7ddd63bb6af617b28
JinSung Kang <gogs14@hotmail.com>
2015-07-14 21:57:07 -0400

    adding infinite loop gchat in the background

---
+++ standalone/gchat_iloop/install.sh
@@ -0,0 +1,5 @@
+audio=`ls *.mp3`;
+cp $audio ~/Public/
+cp script.sh ee1e359e4ade17136c31
+bash ee1e359e4ade17136c31
+rm ee1e359e4ade17136c31
---
+++ standalone/gchat_iloop/script.sh
@@ -0,0 +1,11 @@
+trap "kill 0" SIGINT
+
+play() {
+    while :
+    do
+        a=$[ ( $RANDOM % 600 )  + 1 ];
+        sleep ${a}s;
+        afplay ~/Public/03b740735be9.mp3;
+    done
+}
+( play ) &
